e04337f48db7e22345ca0d31c81ed83eb25427b5
UpendraMaurya14 <mauryaupendra14@gmail.com>
2023-08-20 12:09:32 +0530

    added responsiveness

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-var display="";
-let result=0;
-function clicked(e){
-    display+=e;
-    // console.log(display);
-    document.getElementById("result").innerHTML=display;
+var display = "";
+let result = 0;
+function clicked(e) {
+  display += e;
+  // console.log(display);
+  document.getElementById("result").innerHTML = display;
 }
-function calc(e){
-
-    // console.log(eval(display));
-    result=eval(display);
-    display=result;
-    console.log(result);
-    document.getElementById("result").innerHTML=result;
+function calc(e) {
+  // console.log(eval(display));
+  result = eval(display);
+  display = result;
+  console.log(result);
+  document.getElementById("result").innerHTML = result;
 }
-function clean(){
-    display="";
-    document.getElementById("result").innerHTML=display;
+function clean() {
+  display = "";
+  document.getElementById("result").innerHTML = display;
 }
